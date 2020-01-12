afe3e538ffa7461272a5e3efe03ab63317a0ac70
Mary Lang <mcl86@cornell.edu>
2020-01-12 10:21:56 -0700

    Fill out spec for forecast

--- spec/requests/api/v1/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -6,5 +6,11 @@ describe 'sweater weather api' do
     get "/api/v1/forecast?location=denver,co"
 
     expect(response).to be_successful
+
+    json = JSON.parse(response.body, symbolize_names: true)
+    expect(json).to be_a(Hash)
+    expect(json[:data][:attributes]).to have_key(:hourly_forecast)
+    expect(json[:data][:attributes]).to have_key(:daily_forecast)
+    expect(json[:data][:attributes]).to have_key(:current_weather)
   end
 end
