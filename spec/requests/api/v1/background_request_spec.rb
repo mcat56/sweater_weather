29a63d605d12b53496f5da5f8f55b6d0c7da8ec2
Mary Lang <mcl86@cornell.edu>
2020-01-12 11:00:05 -0700

    Configure vcr for travis

--- spec/requests/api/v1/background_request_spec.rb
+++ spec/requests/api/v1/background_request_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 describe 'sweater weather api' do
   it 'returns background image', :vcr do
+    WebMock.enable_net_connect!
+    VCR.eject_cassette
+    VCR.turn_off!(ignore_cassettes: true)
 
     get "/api/v1/backgrounds?location=denver,co"
 
