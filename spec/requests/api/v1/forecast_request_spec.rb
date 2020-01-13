16ae5fbd3f13578d049122883c2aea0e721a6378
Mary Lang <mcl86@cornell.edu>
2020-01-13 15:19:03 -0700

    Fill out spec for forecast

--- spec/requests/api/v1/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -12,5 +12,24 @@ describe 'sweater weather api' do
     expect(json[:data][:attributes]).to have_key(:hourly_forecast)
     expect(json[:data][:attributes]).to have_key(:daily_forecast)
     expect(json[:data][:attributes]).to have_key(:current_weather)
+
+    expect(json[:data][:attributes][:hourly_forecast].first).to have_key(:humidity)
+    expect(json[:data][:attributes][:hourly_forecast].first).to have_key(:temperature)
+    expect(json[:data][:attributes][:hourly_forecast].first).to have_key(:summary)
+    expect(json[:data][:attributes][:hourly_forecast].first).to have_key(:icon)
+
+    expect(json[:data][:attributes][:daily_forecast]).to have_key(:high)
+    expect(json[:data][:attributes][:daily_forecast]).to have_key(:low)
+    expect(json[:data][:attributes][:daily_forecast]).to have_key(:today)
+    expect(json[:data][:attributes][:daily_forecast]).to have_key(:tonight)
+    expect(json[:data][:attributes][:daily_forecast]).to have_key(:icon)
+
+    expect(json[:data][:attributes][:current_weather]).to have_key(:summary)
+    expect(json[:data][:attributes][:current_weather]).to have_key(:temperature)
+    expect(json[:data][:attributes][:current_weather]).to have_key(:humidity)
+    expect(json[:data][:attributes][:current_weather]).to have_key(:feels_like)
+    expect(json[:data][:attributes][:current_weather]).to have_key(:visibility)
+    expect(json[:data][:attributes][:current_weather]).to have_key(:uvIndex)
+    expect(json[:data][:attributes][:current_weather]).to have_key(:icon)
   end
 end
