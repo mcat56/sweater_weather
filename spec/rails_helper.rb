1442db390ca6e8d7f64783ad59d08aa3bee1f8fe
Mary Lang <mcl86@cornell.edu>
2020-01-12 18:03:48 -0700

    Filter keys

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -22,6 +22,9 @@ VCR.configure do |config|
   config.cassette_library_dir = 'spec/cassettes'
   config.hook_into :webmock
   config.configure_rspec_metadata!
+  config.filter_sensitive_data("<GOOGLE_GEOCODE_API_KEY>") { ENV['GOOGLE_GEOCODE_API_KEY'] } # filter api keys and the like
+  config.filter_sensitive_data("<PEXEL_API_KEY>") { ENV['PEXEL_API_KEY'] } # filter api keys and the like
+  config.filter_sensitive_data("<DARKSKY_API_KEY>") { ENV['DARKSKY_API_KEY'] } # filter api keys and the like
 end
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
