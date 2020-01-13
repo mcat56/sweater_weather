0398dd50d71109708160182c58514bf3b5dda94b
Mary Lang <mcl86@cornell.edu>
2020-01-13 07:38:37 -0700

    Refactor login

--- app/controllers/api/v1/sesssions_controller.rb
+++ app/controllers/api/v1/sesssions_controller.rb
@@ -3,9 +3,11 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     if user.nil?
-      render status: :bad_request, body: user.errors.full_messages.to_sentence
+      render status: :bad_request, body: 'No account found'
     elsif user.authenticate(params[:password])
       render status: :ok, body: { api_key: "#{user.api_key}" }.to_json
+    else
+      render status: :bad_request, body: user.errors.full_messages.to_sentence
     end
   end
 
