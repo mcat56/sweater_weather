43d363c34f2910018ee378b0307de7fc8ed63a3e
Mary Lang <mcl86@cornell.edu>
2020-01-13 21:12:50 -0700

    Refactor future forecast time

--- app/services/dark_sky_service.rb
+++ app/services/dark_sky_service.rb
@@ -5,19 +5,19 @@ class DarkSkyService
   end
 
   def self.get_destination_forecast(coord, travel_time, zone)
-    self.get_json(coord,( ',' + (DateTime.now.in_time_zone(zone) + ((travel_time.to_f)/1440)).to_time.to_s))
+    self.get_json(coord, (',' + (DateTime.now.in_time_zone(zone) + (travel_time/60).minutes).to_s.sub(' ','T').sub(' ', '')) )
   end
 
   private
 
-    def self.connection(coord, date=nil)
+    def self.connection(coord,date = nil)
       Faraday.new(url: "https://api.darksky.net/forecast/#{ENV['DARKSKY_API_KEY']}/#{coord.lat},#{coord.lng}#{date}") do |f|
         f.adapter Faraday.default_adapter
       end
     end
 
-    def self.get_json(coord, date=nil)
-      response = self.connection(coord).get
+    def self.get_json(coord, date = nil)
+      response = self.connection(coord,date).get
       JSON.parse(response.body, symbolize_names: true)
     end
 end
