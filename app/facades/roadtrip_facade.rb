c348d78c0675727b93fe30296d24dab5d601dcef
Mary Lang <mcl86@cornell.edu>
2020-01-14 11:21:35 -0700

    Remove time zone adjust from road trip

--- app/facades/roadtrip_facade.rb
+++ app/facades/roadtrip_facade.rb
@@ -16,12 +16,8 @@ class RoadtripFacade
     @coord ||= Coordinate.new(GoogleGeocodeService.get_coordinates(@destination))
   end
 
-  def time_zone
-    @zone ||= DarkSkyService.get_forecast(destination_coordinates)[:timezone]
-  end
-
   def arrival_forecast
-    forecast = DarkSkyService.get_destination_forecast(destination_coordinates, @time[:value], time_zone)
+    forecast = DarkSkyService.get_destination_forecast(destination_coordinates, @time[:value] )
     arrival_forecast = ArrivalForecast.new(forecast)
     ArrivalForecastPresenter.new(arrival_forecast)
   end
